685567bc1b2a1604b06e01be31dd491c2775b423
Felipe-Santander <felipevillota2002@gmail.com>
2023-11-11 21:10:53 -0500

    ya se creo el cuerpo de la pagina ya se agrego el carrito de compras ahora estoy agregando productos y sus valores

---
+++ scripts.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const productContainer = document.getElementById('product-list');
+    const cartContainer = document.getElementById('cart');
+    const cartList = document.getElementById('cart-list');
+    const cartTotal = document.getElementById('cart-total');
+    const checkoutBtn = document.getElementById('checkout-btn');
+
+    let cartItems = [];
+
+    function updateCart() {
+        cartList.innerHTML = '';
+        let total = 0;
+
+        cartItems.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.className = 'cart-item';
+            listItem.innerHTML = `<span>${item.name}</span><span>$${item.price.toFixed(2)}</span>`;
+            cartList.appendChild(listItem);
+            total += item.price;
+        });
+
+        cartTotal.textContent = total.toFixed(2);
+    }
+
+    function addToCart(name, price) {
+        cartItems.push({ name, price });
+        updateCart();
+    }
+
+    productContainer.addEventListener('click', function (event) {
+        if (event.target.classList.contains('add-to-cart')) {
+            const product = event.target.closest('.product');
+            const productName = product.dataset.name;
+            const productPrice = parseFloat(product.dataset.price);
+            addToCart(productName, productPrice);
+        }
+    });
+
+    checkoutBtn.addEventListener('click', function () {
+        alert(`Compra realizada por $${parseFloat(cartTotal.textContent).toFixed(2)}`);
+        cartItems = [];
+        updateCart();
+    });
+});
